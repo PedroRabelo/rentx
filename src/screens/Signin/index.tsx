83e2c06fe9803cdc35ac7dac742b1844b0078fbc
pedro <pnarabelo@gmail.com>
2021-11-28 11:54:28 -0300

    Tela de login

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,51 +1,84 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native';
 import {
   Container,
   Header,
   Title,
+  Form,
   SubTitle,
   Footer
 } from './styles';
 import {Button} from '../../components/Button';
 import {useTheme} from 'styled-components';
+import {Input} from '../../components/Input';
+import {PasswordInput} from '../../components/PasswordInput';
 
 export function Signin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar
-        barStyle='dark-content'
-        translucent
-        backgroundColor='transparent'
-      />
-      <Header>
-        <Title>
-          Estamos{'\n'}quase lá
-        </Title>
-        <SubTitle>
-          Faça seu login para começar{'\n'}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle='dark-content'
+            translucent
+            backgroundColor='transparent'
+          />
+          <Header>
+            <Title>
+              Estamos{'\n'}quase lá
+            </Title>
+            <SubTitle>
+              Faça seu login para começar{'\n'}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
 
-      <Footer>
-        <Button
-          title='Login'
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button
-          title='Criar conta'
-          color={theme.colors.background_secondary}
-          light
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-      </Footer>
-    </Container>
+          <Form>
+            <Input
+              iconName='mail'
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+              onChangeText={setEmail}
+              value={email}
+            />
+            <PasswordInput
+              iconName='lock'
+              placeholder='Senha'
+              onChangeText={setPassword}
+              value={password}
+            />
+          </Form>
+
+          <Footer>
+            <Button
+              title='Login'
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+            <Button
+              title='Criar conta'
+              color={theme.colors.background_secondary}
+              light
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
